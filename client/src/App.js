61cce5969ec47840aaaf1ee440e782fbf3ced55f
Matt Harris <711mattman@gmail.com>
2021-02-06 13:04:10 -0500

    Nothing special

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./pages/home.js"
-import MyProjects from "./pages/myProjects.js"
+import MyProjects from "./pages/myProjects.js";
 
 function App() {
   return (
+    
     <Router>
       <div>
         <Switch>
