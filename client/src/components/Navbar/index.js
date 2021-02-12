523f53edd9e9bb7c33744f5849cb230126f1389f
RogersJohn811 <rogersjohn811@gmail.com>
2021-02-11 20:29:52 -0500

    Added Flash Grid logo

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./styles.css";
-import logo from "./public/flashgrid.png";
+//import logo from "./public/flashgrid.png";
 
 function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark nav-color">
             <div className="container">
                 <div className="row">
-                    <img> class="logo" src="flashgrid.png"</img>
+                    <img className="logo" src="flashgrid.png"></img>
 
                     <div className="col">
                         <a className="navbar-brand" href="/">
