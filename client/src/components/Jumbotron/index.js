0b942efbfe8af44e347e29eaf00719fd639e4c0c
HishamKader <hisham319@hotmial.com>
2021-03-03 13:22:45 -0500

    changed up styles and fonts

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,7 +4,7 @@ import "./styles.css";
 function Jumbotron() {
   return (<div>
     <div className="jumbotron b">
-      <h1> welcome to</h1>
+      <h1> WELCOME TO</h1>
       <img id="image1" src="flashgrid.png"></img>
       <h3>where projects are made in a flash</h3>
     </div>
