543eaed3864db2107a553cf24447f2e691282ae6
HishamKader <hisham319@hotmial.com>
2021-03-03 17:10:11 -0500

    adde new code

--- client/src/components/ProjectsContainer/index.js
+++ client/src/components/ProjectsContainer/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useContext } from "react";
 import "./style.css";
 import Column from "../Column";
@@ -344,24 +342,17 @@ function ProjectsContainer(props) {
     }
   }
 
-
-// this function runs when click Sav on modal
  function editDataSave(){
      console.log("worked edit") 
-     console.log(editData._id)
-     let id = editData._id
-    API.editTask(id, editData)
-    .then(res => {
-      console.log(res)
-      handleClose() 
-      })
-      }
+     console.log(editData)
+    
 
-// This function sets new data to the state. 
+     }
+
+// 
   function editChangeData(event){
     const newTarget = event.target.name
     const newValue = event.target.value
-     
       console.log(event.target.name)
       console.log(event.target.value)
       
@@ -387,7 +378,7 @@ function ProjectsContainer(props) {
           <table class="table striped bordered hover">
               <thead>
                 <tr>
-                  <th scope="col"><h3 className="align-center">projects</h3></th>
+                  <th scope="col"><h4 className="align-center">Projects</h4></th>
                 </tr>
               </thead>
               <tbody>
@@ -419,11 +410,11 @@ function ProjectsContainer(props) {
             <table class="table striped bordered hover">
               <thead>
                 <tr>
-                  <th scope="col"><h4 className="align-center">task name</h4></th>
-                  <th scope="col"><h4 className="align-center">description</h4></th>
-                  <th scope="col"><h4 className="align-center">assigned personel</h4></th>
-                  <th scope="col"><h4 className="align-center">due date</h4></th>
-                  <th scope="col"><h4 className="align-center">manage</h4></th>
+                  <th scope="col"><h4 className="align-center">Task Name</h4></th>
+                  <th scope="col"><h4 className="align-center">Description</h4></th>
+                  <th scope="col"><h4 className="align-center">Assigned Personel</h4></th>
+                  <th scope="col"><h4 className="align-center">Due Date</h4></th>
+                  <th scope="col"><h4 className="align-center">Manage</h4></th>
                 </tr>
               </thead>
               <tbody>
