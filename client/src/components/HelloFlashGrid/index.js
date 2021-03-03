799f010eb6a6112a34a36b95c2ed511798b8b1b4
HishamKader <hisham319@hotmial.com>
2021-03-02 21:50:36 -0500

    added new styles fixed conflicts

--- client/src/components/HelloFlashGrid/index.js
+++ client/src/components/HelloFlashGrid/index.js
@@ -9,26 +9,26 @@ import { Row, Col } from "react-bootstrap";
 
 function HelloFlashGrid() {
 
-    
+
     return (
         <body>
-            
-    
-                <div id="back" className="container-fluid space-out">
 
-                    <div className="row">
 
-                        <div className="col-sm-8">
-                            <Jumbotron />
-                        </div>
+            <div id="back" className="container-fluid space-out">
 
-                        <div className="col-sm-4" id='sidebar'>
-                            <Sidebar />
-                        </div>
+                <div className="row">
 
+                    <div className="col-sm-8">
+                        <Jumbotron />
                     </div>
+
+                    <div className="col-sm-4" id='sidebar'>
+                        <Sidebar />
+                    </div>
+
                 </div>
-            
+            </div>
+
         </body>
     );
 }
